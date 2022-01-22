93125383e626fa29faa17ac59445b491bf48b6b3
Masoud Hashemi <smasoud.hashemi@gmail.com>
2022-01-22 17:01:50 -0500

    initial commit

--- sensr.py
+++ sensr.py
@@ -1,8 +1,8 @@
 import numpy as np
-from sklearn.decomposition import TruncatedSVD
-from sklearn.linear_model import LogisticRegression
 import torch
 import torch.nn as nn
+from sklearn.decomposition import TruncatedSVD
+from sklearn.linear_model import LogisticRegression
 
 
 def calc_sensitive_directions(x_no_sensitive, sensitive_group):
@@ -12,7 +12,7 @@ def calc_sensitive_directions(x_no_sensitive, sensitive_group):
     """
     sensitive_directions = []
     for y_protected in sensitive_group.T:
-        lr = LogisticRegression(solver='liblinear', fit_intercept=True)
+        lr = LogisticRegression(solver="liblinear", fit_intercept=True)
         lr.fit(x_no_sensitive, y_protected)
         sensitive_directions.append(lr.coef_.flatten())
 
@@ -33,8 +33,8 @@ def compl_svd_projector(sensitive_directions, svd=-1):
         tSVD.fit(sensitive_directions)
         basis = tSVD.components_.T
     else:
-        basis = sensitive_directions.T    
-    
+        basis = sensitive_directions.T
+
     proj = normalize_sensitive_directions(basis)
     proj_compl = np.eye(proj.shape[0]) - proj
     return proj_compl
@@ -42,7 +42,9 @@ def compl_svd_projector(sensitive_directions, svd=-1):
 
 def fair_dist(proj_compl):
     proj_compl_ = torch.FloatTensor(proj_compl)
-    return lambda x, y: torch.sum(torch.square(torch.matmul(x-y,proj_compl_)), dim=1)
+    return lambda x, y: torch.sum(
+        torch.square(torch.matmul(x - y, proj_compl_)), dim=1
+    )
 
 
 def unprotected_direction(x, sensetive_directions):
@@ -50,17 +52,31 @@ def unprotected_direction(x, sensetive_directions):
     return x
 
 
-def sample_perturbation(model, x, y, sensetive_directions, regularizer = 100, learning_rate = 5e-2, num_steps = 200):
+def sample_perturbation(
+    model,
+    x,
+    y,
+    sensetive_directions,
+    regularizer=100,
+    learning_rate=5e-2,
+    num_steps=200,
+):
     x_start = x.clone()
-    x.requires_grad=True
-    x_start.requires_grad=False
-    sensetive_directions_ = normalize_sensitive_directions(sensetive_directions)
+    x_start.requires_grad = False
+    x_ = x.clone()
+    x_.requires_grad = True
+    sensetive_directions_ = normalize_sensitive_directions(
+        sensetive_directions
+    )
     for i in range(num_steps):
-        prob = model(x)
-        perturb = unprotected_direction(x-x_start, sensetive_directions_)
-        loss = nn.CrossEntropyLoss(y, prob)  - regularizer  * torch.norm(perturb)**2
-        gradient = torch.autograd.gradient(loss, x)
-        x = x + learning_rate * gradient
-        x_start = x.clone()
-        x_start.requires_grad=False
-    return x
+        prob = model(x_)
+        perturb = unprotected_direction(x_ - x_start, sensetive_directions_)
+        loss = (
+            nn.CrossEntropyLoss()(prob, y)
+            - regularizer * torch.linalg.norm(perturb) ** 2
+        )
+        gradient = torch.autograd.gradient(loss, x_)
+        x_ = x_ + learning_rate * gradient
+        x_start = x_.clone()
+        x_start.requires_grad = False
+    return x_
